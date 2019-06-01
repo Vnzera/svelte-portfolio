3ffe1c122048d34f4ef0d9fb2b6d991bac7fe0ff
Luis Diaz <luis@linuxmail.org>
2019-06-01 09:53:31 -0700

    Do not need the extra files. Kept them only to easily reference them

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,6 @@
 <script>
   import Header from "./Header.svelte";
-  import Cart from "./Cart.svelte";
-  import Product from "./Product.svelte";
   import Button from "./Button.svelte";
-
-  let title = "";
-  let price = 0;
-  let description = "";
-
-  let products = [];
-  let cartItems = [];
-
-  function setTitle(event) {
-    title = event.target.value;
-  }
-
-  function createProduct() {
-    const newProduct = {
-      title: title,
-      price: price,
-      description: description
-    };
-
-    products = products.concat(newProduct);
-  }
-
-  function addToCart(event) {
-    const selectedTitle = event.detail;
-    cartItems = cartItems.concat({
-      ...products.find(prod => prod.title === selectedTitle)
-    });
-    console.log(cartItems);
-  }
 </script>
 
 <style>
@@ -48,40 +17,3 @@
 </style>
 
 <Header />
-
-<section>
-  <Cart items={cartItems} />
-</section>
-
-<hr />
-
-<section>
-  <div>
-    <label for="title">Title</label>
-    <input type="text" id="title" value={title} on:input={setTitle} />
-  </div>
-  <div>
-    <label for="price">Price</label>
-    <input type="number" id="title" bind:value={price} />
-  </div>
-  <div>
-    <label for="description">Description</label>
-    <textarea rows="3" id="description" bind:value={description} />
-  </div>
-
-  <Button on:click={createProduct}>Create Product</Button>
-</section>
-
-<section>
-  {#if products.length === 0}
-    <p>No products have been added yet!</p>
-  {:else}
-    {#each products as product}
-      <Product
-        productTitle={product.title}
-        productPrice={product.price}
-        productDescription={product.description}
-        on:addcart={addToCart} />
-    {/each}
-  {/if}
-</section>
