b697cca30e2a413ac3b02afb92d50f315f4b9115
Luis Diaz <luis@linuxmail.org>
2019-06-06 13:15:46 -0700

    Added About page

---
+++ src/About.svelte
@@ -0,0 +1,30 @@
+<style>
+  section {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+    height: 50vh;
+    margin: 10vh 5vh 0vh 5vh;
+  }
+
+  div {
+    text-align: center;
+  }
+
+  img {
+    border: 1.5px solid black;
+    border-radius: 50%;
+    height: 15rem;
+    width: 15rem;
+  }
+</style>
+
+<section>
+  <img
+    alt="me"
+    src="https://avatars0.githubusercontent.com/u/16928531?s=460&v=4" />
+  <div>
+    <h1>Full Stack JavaScript</h1>
+    <p>Svelte.js, React.js, Node.js and MongoDB</p>
+  </div>
+</section>
--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,15 @@
 <script>
   import Header from "./Header.svelte";
+  import About from "./About.svelte";
   import Projects from "./Projects.svelte";
   // import Services from "./Services.svelte";
-  // import About from "./About.svelte";
   // import Contact from "./Contact.svelte";
 
-  import Button from "./Button.svelte";
+  // import Button from "./Button.svelte";
 </script>
 
 <style>
-  section {
+  /* section {
     width: 30rem;
     margin: auto;
   }
@@ -18,8 +18,9 @@
   label,
   textarea {
     width: 100%;
-  }
+  } */
 </style>
 
 <Header />
+<About />
 <Projects />
