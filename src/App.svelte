dcaf8684281987f22b678b5d8fe6f8b111e9dc18
Luis Diaz <luis@linuxmail.org>
2019-07-08 14:59:33 -0700

    Not showing up on netlify

--- src/App.svelte
+++ src/App.svelte
@@ -4,8 +4,6 @@
   import Projects from "./Projects.svelte";
   import Services from "./Services.svelte";
   import Contact from "./Contact.svelte";
-
-  // import Button from "./Button.svelte";
 </script>
 
 <style>
@@ -15,12 +13,6 @@
   /* section {
     width: 30rem;
     margin: auto;
-  }
-
-  input,
-  label,
-  textarea {
-    width: 100%;
   } */
 </style>
 
